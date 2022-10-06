28daaa7dc3221b30445af8ac512cbdecb90af457
slanow831 <s.nowak3@gmail.com>
2022-10-06 11:26:40 +0200

    dddddd

--- biling.ps1
+++ biling.ps1
@@ -4,7 +4,7 @@ $end = (get-date).ToString("yyyy-MM-dd")
 $start_date = "'start_date'"
 $end_date = "'end_date'"
 
-$azure_billing_data = Get-AzConsumptionUsageDetail -ResourceGroup ing-hackathon-rg -StartDate $start -EndDate $end | sort PretaxCost -Descending | Group-Object InstanceName | Select-Object name, @{ n='Cost(EUR)'; e={ [math]::Round((($_.Group | Measure-Object 'PretaxCost' -Sum).Sum),2) } }, @{ n='Count'; e={$_.count } }, @{ n='CostAV(EUR)'; e={ [math]::Round((($_.Group | Measure-Object 'PretaxCost' -Average).Average ),2) }}
+$azure_billing_data = Get-AzConsumptionUsageDetail -ResourceGroup ing-hackathon-rg -StartDate $start -EndDate $end | sort PretaxCost -Descending | Group-Object InstanceName | Select-Object name, @{ n='Count'; e={$_.count } }, @{ n='Cost(EUR)'; e={ [math]::Round((($_.Group | Measure-Object 'PretaxCost' -Sum).Sum),2) } }, @{ n='CostAV(EUR)'; e={ [math]::Round((($_.Group | Measure-Object 'PretaxCost' -Average).Average ),2) }}
 $azure_billing_data |Export-Csv  -NoTypeInformation -Path ".\billing.csv" -Delimiter ';'
 $cleaner = Get-Content .\billing_db.csv
 $cleaner.Replace('","',",").TrimStart('"').TrimEnd('"') | Out-File .\billing_db.csv -Force -Confirm:$false
